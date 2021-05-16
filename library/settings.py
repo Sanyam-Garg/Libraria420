620af8c152f04f2b174801acb2ad18ea7e19c74e
Sanyam <shanujindal48@gmail.com>
2021-05-16 23:59:08 +0530

    .

--- library/settings.py
+++ library/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = keys.SECRET_KEY
+SECRET_KEY = '8$ihhjp#+-v_ti8!8x353+#!wt#z2%l!nsxgqb#04t5^!x)ui8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
